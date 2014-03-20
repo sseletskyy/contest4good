b51e823aaadcd40b870c6c6cafd2473df8067f1c
Serge Seletskyy <sergey@clcoudconsulting.no>
2014-03-21 00:32:37 +0200

    set requirements for updating contest in capybara spec

--- spec/features/a/update_contest_spec.rb
+++ spec/features/a/update_contest_spec.rb
@@ -23,6 +23,7 @@ feature "Update Contest" do
       @admin = create_current_admin
       @committee_vice = fg.create(:committee_vice)
       @jury_judge = fg.create(:jury_judge)
+      @jury_judge_2 = fg.create(:jury_judge)
 
     end
     context "when admin has role admin" do
@@ -37,7 +38,7 @@ feature "Update Contest" do
 
       context "check for fields in the form" do
         scenario 'all elements should be available' do
-          %w(name starts_at end_at regulations committee_head_ids jury_head_ids committee_vice_ids jury_judge_ids).each do |field|
+          %w(name starts_at ends_at regulations committee_head_ids jury_head_ids committee_vice_ids jury_judge_ids).each do |field|
             expect { find_field("contest_#{field}") }.to be_true
           end
         end
@@ -68,7 +69,8 @@ feature "Update Contest" do
           expect(page).to have_content I18n.t('errors.messages.blank')
         end
 
-        scenario "committee_vice" do
+        scenario "committee_vice is required if it was set before" do
+          @committee_vice.add_role Contest4good::ROLE_COMMITTEE_VICE, @contest
           fill_name('contest name')
           fill_correct_dates
           fill_regulations('regulation text')
@@ -78,7 +80,18 @@ feature "Update Contest" do
           expect(page).to have_content I18n.t('errors.messages.blank')
         end
 
-        scenario "jury_judge" do
+        scenario "committee_vice is not required if it wasn't set before" do
+          fill_name('contest name')
+          fill_correct_dates
+          fill_regulations('regulation text')
+          fill_jury_judge(@jury_judge)
+          submit
+          current_path.should == a_contests_path
+          expect(page).to have_content I18n.t('a.contests.notices.create')
+        end
+
+        scenario "jury_judge is required if it was set before" do
+          @jury_judge.add_role Contest4good::ROLE_JURY_JUDGE, @contest
           fill_name('contest name')
           fill_correct_dates
           fill_regulations('regulation text')
@@ -87,6 +100,17 @@ feature "Update Contest" do
           submit
           current_path.should == a_contest_path(@contest)
           expect(page).to have_content I18n.t('errors.messages.blank')
+        end
+
+        scenario "jury_judge is not required if it was not set before" do
+          fill_name('contest name')
+          fill_correct_dates
+          fill_regulations('regulation text')
+          fill_committee_vice(@committee_vice)
+
+          submit
+          current_path.should == a_contests_path
+          expect(page).to have_content I18n.t('a.contests.notices.create')
         end
       end
 
@@ -125,12 +149,251 @@ feature "Update Contest" do
           end
         end
       end
+    end
 
-      # TODO context "when admin has role committee_head" do
-      # TODO context "when admin has role committee_vice" do
-      # TODO context "when admin has role jury_head" do
-      # TODO context "when admin has role jury_judge" do
+    context "when admin has role committee_head" do
+      background do
+        @admin.add_role Contest4good::ROLE_COMMITTEE_HEAD, @contest
+        visit edit_a_contest_path(@contest)
+      end
+      scenario "edit form is available" do
+        current_path.should == edit_a_contest_path(@contest)
+      end
 
+      context "check for field in the form is" do
+        context 'available' do
+          scenario 'committee_vice_ids' do
+            expect { find_field("contest_committee_vice_ids") }.to be_true
+          end
+        end
+        context 'NOT available' do
+          scenario 'name' do
+            expect { find_field("contest_name") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'starts_at' do
+            expect { find_field("contest_starts_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'ends_at' do
+            expect { find_field("contest_ends_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'regulations' do
+            expect { find_field("contest_regulations") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'committee_head_ids' do
+            expect { find_field("contest_committee_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'jury_head_ids' do
+            expect { find_field("contest_jury_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'jury_judge_ids' do
+            expect { find_field("contest_jury_judge_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+        end
+      end
+
+      context "validate required field" do
+        scenario "committee_vice" do
+          fill_committee_vice(@committee_vice)
+          submit
+          current_path.should == a_contests_path
+          expect(page).to have_content I18n.t('a.contests.notices.create')
+        end
+      end
+
+      context "contest" do
+        background do
+          fill_committee_vice(@committee_vice)
+          submit
+        end
+
+        scenario 'should be updated' do
+          expect(page).to have_content I18n.t('a.contests.notices.create')
+          current_path.should == a_contests_path
+        end
+
+        scenario 'email should be sent to committee_vice' do
+          emails_to_test = [
+              {email: @committee_vice.email, role: Contest4good::ROLE_COMMITTEE_VICE}
+          ]
+
+          emails_to_test.each do |item|
+            puts "\t~ Checking email #{item[:email]} and role #{item[:role]}"
+            email = open_email(item[:email])
+            mail_body = email.body.encoded
+            #puts mail_body
+            expect(mail_body).to have_content I18n.t('mailer.contest.invite_mail_subject')
+            translated_role = I18n.t(item[:role], scope: 'simple_form.options.admin.roles')
+            your_role_is_html = ActionView::Base.full_sanitizer.sanitize(I18n.t('mailer.contest.your_role_is_html', role: translated_role, contest: Contest.last.name))
+            expect(mail_body).to have_content your_role_is_html
+          end
+        end
+      end
+    end
+
+    context "when admin has role committee_vice" do
+      background do
+        @admin.add_role Contest4good::ROLE_COMMITTEE_VICE, @contest
+        visit edit_a_contest_path(@contest)
+      end
+      scenario "edit form is available" do
+        current_path.should == edit_a_contest_path(@contest)
+      end
+
+      context "check for field in the form is" do
+        context 'available' do
+          scenario 'committee_vice_ids' do
+            # no fields are available !
+            #expect { find_field("contest_committee_vice_ids") }.to be_true
+          end
+        end
+        context 'NOT available' do
+          scenario 'name' do
+            expect { find_field("contest_name") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'starts_at' do
+            expect { find_field("contest_starts_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'ends_at' do
+            expect { find_field("contest_ends_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'regulations' do
+            expect { find_field("contest_regulations") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'committee_head_ids' do
+            expect { find_field("contest_committee_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'committee_vice_ids' do
+            expect { find_field("contest_committee_vice_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'jury_head_ids' do
+            expect { find_field("contest_jury_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'jury_judge_ids' do
+            expect { find_field("contest_jury_judge_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+        end
+      end
+
+      #context "validate required field" do
+      #  scenario "committee_vice" do
+      #    submit
+      #    current_path.should == a_contests_path
+      #    expect(page).to have_content I18n.t('a.contests.notices.create')
+      #  end
+      #end
+
+      #context "contest" do
+      #  background do
+      #    submit
+      #  end
+      #
+      #  scenario 'should be updated' do
+      #    expect(page).to have_content I18n.t('a.contests.notices.create')
+      #    current_path.should == a_contests_path
+      #  end
+      #
+      #  scenario 'email should be sent to committee_vice' do
+      #    emails_to_test = [
+      #        {email: @committee_vice.email, role: Contest4good::ROLE_COMMITTEE_VICE}
+      #    ]
+      #
+      #    emails_to_test.each do |item|
+      #      puts "\t~ Checking email #{item[:email]} and role #{item[:role]}"
+      #      email = open_email(item[:email])
+      #      mail_body = email.body.encoded
+      #      #puts mail_body
+      #      expect(mail_body).to have_content I18n.t('mailer.contest.invite_mail_subject')
+      #      translated_role = I18n.t(item[:role], scope: 'simple_form.options.admin.roles')
+      #      your_role_is_html = ActionView::Base.full_sanitizer.sanitize(I18n.t('mailer.contest.your_role_is_html', role: translated_role, contest: Contest.last.name))
+      #      expect(mail_body).to have_content your_role_is_html
+      #    end
+      #  end
+      #end
+    end
+
+    context "when admin has role jury_head" do
+      background do
+        #ActionMailer::Base.deliveries = []
+        @admin.add_role Contest4good::ROLE_JURY_HEAD, @contest
+        visit edit_a_contest_path(@contest)
+      end
+      scenario "edit form is available" do
+        current_path.should == edit_a_contest_path(@contest)
+      end
+
+      context "check for field in the form is" do
+        context 'available' do
+          scenario 'jury_judge_ids' do
+            expect { find_field("contest_jury_judge_ids") }.to be_true
+          end
+        end
+        context 'NOT available' do
+          scenario 'name' do
+            expect { find_field("contest_name") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'starts_at' do
+            expect { find_field("contest_starts_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'ends_at' do
+            expect { find_field("contest_ends_at") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'regulations' do
+            expect { find_field("contest_regulations") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'committee_head_ids' do
+            expect { find_field("contest_committee_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+          scenario 'jury_head_ids' do
+            expect { find_field("contest_jury_head_ids") }.to raise_error(Capybara::ElementNotFound)
+          end
+        end
+      end
+
+      context "validate required field" do
+        scenario "jury_judge is required if it was set before" do
+          @jury_judge.add_role Contest4good::ROLE_JURY_JUDGE, @contest
+          submit
+          current_path.should == a_contest_path(@contest)
+          expect(page).to have_content I18n.t('errors.messages.blank')
+        end
+
+        scenario "jury_judge is not required if it was not set before" do
+          submit
+          current_path.should == a_contests_path
+          expect(page).to have_content I18n.t('a.contests.notices.create')
+        end
+      end
+
+      context "contest" do
+        background do
+          fill_jury_judge(@jury_judge)
+          fill_jury_judge(@jury_judge_2)
+          submit
+        end
+
+        scenario 'should be updated' do
+          expect(page).to have_content I18n.t('a.contests.notices.create')
+          current_path.should == a_contests_path
+        end
+
+        scenario 'email should be sent to jury_judge and jury_judge_2' do
+          emails_to_test = [
+              {email: @jury_judge.email, role: Contest4good::ROLE_JURY_JUDGE},
+              {email: @jury_judge_2.email, role: Contest4good::ROLE_JURY_JUDGE}
+          ]
+
+          emails_to_test.each do |item|
+            puts "\t~ Checking email #{item[:email]} and role #{item[:role]}"
+            email = open_email(item[:email])
+            mail_body = email.body.encoded
+            #puts mail_body
+            expect(mail_body).to have_content I18n.t('mailer.contest.invite_mail_subject')
+            translated_role = I18n.t(item[:role], scope: 'simple_form.options.admin.roles')
+            your_role_is_html = ActionView::Base.full_sanitizer.sanitize(I18n.t('mailer.contest.your_role_is_html', role: translated_role, contest: Contest.last.name))
+            expect(mail_body).to have_content your_role_is_html
+          end
+        end
+      end
     end
 
   end
